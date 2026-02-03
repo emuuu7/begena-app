781780b523299b9ecb552b8c260054c60d0159c1
emuuu07 <temesegenm02@gmail.com>
2026-02-03 16:33:46 +0300

    made the strings perfectly alligned with the sound and make vibrations

--- src/components/Begena.jsx
+++ src/components/Begena.jsx
@@ -1,16 +1,25 @@
 import { useEffect } from "react";
 const strings = [
- {id: 2, sound: "/sounds/kuter2.mp3", left: "41%" },
- {id: 3, sound: "/sounds/kuter3.mp3", left: "47%" },
- {id: 1, sound: "/sounds/kuter1.mp3", left: "34%" },
- {id: 5, sound: "/sounds/kuter5.mp3", left: "57%" },
- {id: 4, sound: "/sounds/kuter4.mp3", left: "52%" }
+ {id: 2, sound: "/sounds/kuter2.mp3", left: "44.5%", angle: -2 },
+ {id: 3, sound: "/sounds/kuter3.mp3", left: "49%", angle: -0.5 },
+ {id: 1, sound: "/sounds/kuter1.mp3", left: "37%",angle: -3.3 },
+ {id: 5, sound: "/sounds/kuter5.mp3", left: "57.5%", angle:0.8 },
+ {id: 4, sound: "/sounds/kuter4.mp3", left: "54%", angle:0.7  }
 ];
 function playSound(src) {
   const audio = new Audio(src);
   audio.currentTime = 0;
   audio.play();
 }
+function vibrateString(id) {
+  const el = document.getElementById(`string-${id}`);
+  if (!el) return;
+
+  el.classList.add("vibrate");
+  void el.offsetHeight;
+  setTimeout(() => el.classList.remove("vibrate"), 10);
+}
+
 export default function Begena() {
     useEffect(() => {
         const keyMap = {
@@ -32,12 +41,23 @@ export default function Begena() {
         return () => window.removeEventListener("keydown", handleKeyDown);
     }, []);
     return (
-        <div className="begena-container">
+        <div
+          style={{
+    position: "relative",
+    width: "100%",
+    maxWidth: "420px",
+    aspectRatio: "3 / 5",   // adjust to match your image
+    margin: "0 auto",
+  }}>
             <h2 style={{ textAlign: "center"}}>Play the Begena</h2>
             <img
                 src="/public/begenaimg.jpg"
                 alt="Begena"
-                style={{width: "100%", display : "block"}}
+                style={{width: "100%",
+                          height: "100%",
+      objectFit: "contain",
+      display: "block",
+                }}
                 />
             {/* use strings array to create buttons for each string 
             {strings.map((string) => (
@@ -50,17 +70,24 @@ export default function Begena() {
             ))} */}
                   {/* Strings overlay */}
       {strings.map((string) => (
-        <div
+        <div 
+            id ={`string-${string.id}`}
           key={string.id}
-          onClick={() => playSound(string.sound)}
+          onClick={() =>{
+            playSound(string.sound);
+            vibrateString(string.id);
+          } }
           style={{
-            backgroundColor: "rgba(0, 0, 0, 0.05)",
+            backgroundColor: "rgba(0, 0, 0, 0.1)",
             position:" absolute",
-            top: "0",
+            top: "18%",
             left: string.left,
-            width: "2%",
-            height: "200%",
+            width: "3px",
+            height: "76%",
+           transform: `rotate(${string.angle}deg)`,
+            transformOrigin: "top center",
             cursor: "pointer",
+ 
           }}
         />
       ))}
